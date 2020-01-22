0427ed625044afc135a32eb1be8fbf41395e46e7
dominikp <eukida>
2020-01-22 17:18:01 +0100

    fix: calculate 2 or more numbers in salary field

--- ForumEarner/validation/salary_validator.py
+++ ForumEarner/validation/salary_validator.py
@@ -31,48 +31,52 @@ def valid_salary(content):
 
 def get_salary(salary, contract_type):
     global currency, taxes
-    if re.search(r'.*?(\d+)(\.|,|\s|\')(\d+).*?', salary):
-        if re.search(r'.*?(\d+)(\.|,)(\d+)(|\s)k.*?', salary):
-            first_part = re.search(r'.*?(\d+)(\.|,)(\d+)(|\s)k.*?', salary).group(1)
-            second_part = re.search(r'.*?(\d+)(\.|,)(\d+)(|\s)k.*?', salary).group(3)
+    if re.search(r'(.*?|^)(\d+)(\.|,|\s|\')(\d+).*?', salary):
+        if re.search(r'(.*?|^)(\d+)(\.|,)(\d+)(|\s)k.*?', salary):
+            first_part = re.search(r'(.*?|^)(\d+)(\.|,)(\d+)(|\s)k.*?', salary).group(2)
+            second_part = re.search(r'(.*?|^)(\d+)(\.|,)(\d+)(|\s)k.*?', salary).group(4)
             contract_salary = int(first_part) * 1000 + int(second_part) * 100
-        elif re.search(r'.*?(\d+)(\.|,)(\d+).*?(tyś|tys).*?', salary):
-            first_part = re.search(r'.*?(\d+)(\.|,)(\d+).*?(tyś|tys).*?', salary).group(1)
-            second_part = re.search(r'.*?(\d+)(\.|,)(\d+).*?(tyś|tys).*?', salary).group(3)
+        elif re.search(r'(.*?|^)(\d+)(\.|,)(\d+).{1,5}(tyś|tys|tysiecy).*?', salary):
+            first_part = re.search(r'(.*?|^)(\d+)(\.|,)(\d+).{1,5}(tyś|tys|tysiecy).*?', salary).group(2)
+            second_part = re.search(r'(.*?|^)(\d+)(\.|,)(\d+).{1,5}(tyś|tys|tysiecy).*?', salary).group(4)
             contract_salary = int(first_part) * 1000 + int(second_part) * 100
-        elif re.search(r'.*?(\d+)(\.|,)(\d+).*?(/h|/ h|/g|/ g|godz.*|rbh).*?', salary):
-            first_part = re.search(r'.*?(\d+)(\.|,)(\d+).*?(/h|/ h|/g|/ g|godz.*|rbh).*?', salary).group(1)
-            second_part = re.search(r'.*?(\d+)(\.|,)(\d+).*?(/h|/ h|/g|/ g|godz.*|rbh).*?', salary).group(3)
+        elif re.search(r'(.*?|^)(\d+)(\.|,)(\d+).{1,5}(/h|/ h|/g|/ g|godz|godzine|rbh).*?', salary):
+            first_part = re.search(r'(.*?|^)(\d+)(\.|,)(\d+).{1,5}(/h|/ h|/g|/ g|godz|godzine|rbh).*?', salary).group(2)
+            second_part = re.search(r'(.*?|^)(\d+)(\.|,)(\d+).{1,5}(/h|/ h|/g|/ g|godz|godzine|rbh).*?', salary).group(4)
             contract_salary = int(first_part) * 160 + int(second_part) / 10 * 100
-        elif re.search(r'.*?(\d+)(\.|,)(\d+).*?(dzien.*|dzień|per day|md|/d|/ d).*?', salary):
-            first_part = re.search(r'.*?(\d+)(\.|,)(\d+).*?(dzien.*|dzień|per day|md|/d|/ d).*?', salary).group(1)
-            second_part = re.search(r'.*?(\d+)(\.|,)(\d+).*?(dzien.*|dzień|per day|md|/d|/ d).*?', salary).group(3)
+        elif re.search(r'(.*?|^)(\d+)(\.|,)(\d+).{1,5}(dzien|dziennie|dzień|per day|md|/d|/ d).*?', salary):
+            first_part = re.search(r'(.*?|^)(\d+)(\.|,)(\d+).{1,5}(dzien|dziennie|dzień|per day|md|/d|/ d).*?', salary).group(2)
+            second_part = re.search(r'(.*?|^)(\d+)(\.|,)(\d+).{1,5}(dzien|dziennie|dzień|per day|md|/d|/ d).*?', salary).group(4)
             contract_salary = int(first_part) * 20 + int(second_part) * 10
         else:
-            first_part = str(re.search(r'.*?(\d+)(\.|,|\s|\')(\d+).*?', salary).group(1))
-            second_part = str(re.search(r'.*?(\d+)(\.|,|\s|\')(\d+).*?', salary).group(3))
+            first_part = str(re.search(r'(.*?|^)(\d+)(\.|,|\s|\')(\d+).*?', salary).group(2))
+            second_part = str(re.search(r'(.*?|^)(\d+)(\.|,|\s|\')(\d+).*?', salary).group(4))
             contract_salary = int(first_part + second_part)
 
             if contract_salary < 100:
                 contract_salary *= 100
     else:
-        if re.search(r'.*?(\d+)(|\s)k.*?', salary):
-            if 'rok' in salary or 'rocznie' in salary:
-                contract_salary = int(re.search(r'.*?(\d+)(|\s)k.*?', salary).group(1)) * 1000
-                contract_salary /= 12
+        if len(re.findall(r'(\s|^)(\d+)', salary)) < 2 and re.search(r'(\s|^)(\d+).*?\dk.*', salary) is None:
+            if re.search(r'(.*?|^)(\d+).{0,2}k.*?', salary):
+                if 'rok' in salary or 'rocznie' in salary:
+                    contract_salary = int(re.search(r'(.*?|^)(\d+).{0,2}k.*?', salary).group(2)) * 1000
+                    contract_salary /= 12
+                else:
+                    contract_salary = int(re.search(r'(.*?|^)(\d+).{0,2}k.*?', salary).group(2)) * 1000
+            elif re.search(r'(.*?|^)(\d+).{1,5}(tyś|tys|tysiecy).*?', salary):
+                contract_salary = int(re.search(r'(.*?|^)(\d+).{1,5}(tyś|tys|tysiecy).*?', salary).group(2)) * 1000
+            elif re.search(r'(.*?|^)(\d+).{1,5}(/h|/ h|/g|/ g|godz|godzine|rbh).*?', salary):
+                contract_salary = int(re.search(r'(.*?|^)(\d+).{1,5}(/h|/ h|/g|/ g|godz|godzine|rbh).*?', salary).group(2)) * 160
+            elif re.search(r'(.*?|^)(\d+).{1,5}(dzien|dziennie|dzień|per day|md|/d|/ d).*?', salary):
+                contract_salary = int(re.search(r'(.*?|^)(\d+).{1,5}(dzien|dziennie|dzień|per day|md|/d|/ d).*?', salary).group(2)) * 20
             else:
-                contract_salary = int(re.search(r'.*?(\d+)(|\s)k.*?', salary).group(1)) * 1000
-        elif re.search(r'.*?(\d+).*?(tyś|tys.*).*?', salary):
-            contract_salary = int(re.search(r'.*?(\d+).*?(tyś|tys.*).*?', salary).group(1)) * 1000
-        elif re.search(r'.*?(\d+).*?(/h|/ h|/g|/ g|godz.*|rbh).*?', salary):
-            contract_salary = int(re.search(r'.*?(\d+).*?(/h|/ h|/g|/ g|godz.*|rbh).*?', salary).group(1)) * 160
-        elif re.search(r'.*?(\d+).*?(dzien.*|dzień|per day|md|/d|/ d).*?', salary):
-            contract_salary = int(re.search(r'.*?(\d+).*?(dzien.*|dzień|per day|md|/d|/ d).*?', salary).group(1)) * 20
-        else:
-            contract_salary = int(re.search(r'.*?(\d+).*', salary).group(1))
+                contract_salary = int(re.search(r'(.*?|^)(\d+).*', salary).group(2))
 
-    if contract_salary > 50000 or contract_salary < 800:
-        contract_salary = None
+            if contract_salary > 80000 or contract_salary < 800:
+                contract_salary = None
+        else:
+            print('hej ' + salary)
+            contract_salary = int(re.search(r'(.*?|^)(\d+).*?', salary).group(2))
 
     if contract_salary is not None:
 
@@ -98,7 +102,8 @@ def get_salary(salary, contract_type):
             if 'b2b' in contract_type:
                 taxes = 'netto'
             else:
+                if 'uop' in contract_type and (contract_salary % 1000 == 0 or contract_salary > 8000):
+                    taxes = 'brutto'
                 taxes = '!'
-
 
     return [contract_salary, currency, taxes, contract_type]
